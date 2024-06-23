899dfc1b6e58ee07aac702269e7939678bc8cff4
aldo garcia <aldoredcreo@gmail.com>
2024-06-22 20:11:17 -0600

    Inico del Proyecto,Crea formulario, archivos View,

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from 'vue-router';
+
+import headerItem from './components/header-item.vue';
+</script>
+
+<template>
+ <headerItem/>
+ <div>
+    <RouterView
+    
+    />
+ </div>
+
+</template>
---
+++ src/components/header-item.vue
@@ -0,0 +1,40 @@
+<script setup>
+</script>
+
+<template>
+    <div class="mx-auto container px-5 py-16">
+        <div>
+
+        </div>
+        <form 
+            class="md:w-1/2 2xl:w-1/3 bg-orange-400 py-31 p-10 drop-shadow-xl space-y-4"
+        >
+        <div class="space-y-2">
+            <label for="ingrediente"
+                class="block text-white text-lg uppercase ">
+                Nombre o Ingrediente
+            </label>
+            <input 
+                class="rounded-lg focus:outline-none w-full"
+            type="text">
+        </div>
+
+        <div class="space-y-2">
+            <label for="ingrediente"
+                class="block text-white text-lg uppercase ">
+                Categoria
+            </label>
+            <select class="rounded-lg focus:outline-none w-full text-center" name="" id="">
+                <option value="">-- Selecciona --</option>
+            </select>
+        </div>
+
+        <input 
+        class="bg-orange-800 hover:bg-orange-900 w-full text-white py-1 rounded-2xl"
+        type="submit"
+        value="Buscar Bebida"
+        >
+        </form>
+
+    </div>
+</template>
